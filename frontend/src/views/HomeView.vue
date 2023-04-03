846ee67cb925af6b8d016bdb028a6db036b384db
ElPountie <khalis.rabehi@etu.univ-lyon1.fr>
2023-04-03 15:03:48 +0200

    Revert "feat : add home page with products, search bar and sorter"
    This reverts commit 6ccaa40153a8626e08bec6d941622b0bbb8fde44.

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -3,41 +3,18 @@ import { ref, computed } from "vue";
 
 const loading = ref(false);
 const error = ref(false);
-const filter = ref("");
-const sorter = ref("name");
-const products = ref([]);
 
 async function fetchProducts() {
   loading.value = true;
   error.value = false;
 
-  try{
-    const response = await fetch("http://localhost:3000/api/products");
-    products.value = await response.json();
+  try {
   } catch (e) {
     error.value = true;
   } finally {
     loading.value = false;
   }
 }
-
-const filteredProducts = computed(() =>
-  products.value.filter((product) =>
-    product.name.toLowerCase().includes(filter.value.toLowerCase())
-  )
-);
-
-const sortedProducts = computed(() =>
-  filteredProducts.value.sort((a, b) => {
-    if (sorter.value === "name") {
-      return a.name.localeCompare(b.name);
-    } else if (sorter.value === "price") {
-      return a.originalPrice - b.originalPrice;
-    } else {
-      return 0;
-    }
-  })
-);
 
 fetchProducts();
 </script>
@@ -51,24 +28,32 @@ fetchProducts();
         <form>
           <div class="input-group">
             <span class="input-group-text">Filtrage</span>
-            <input v-model="filter" type="text" class="form-control" placeholder="Filtrer par nom" data-test-filter />
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Filtrer par nom"
+              data-test-filter
+            />
           </div>
         </form>
       </div>
       <div class="col-md-6 text-end">
         <div class="btn-group">
-          <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"
-            data-test-sorter>
-            Trier par {{ sorter === "name" ? "nom" : "prix" }}
+          <button
+            type="button"
+            class="btn btn-primary dropdown-toggle"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+            data-test-sorter
+          >
+            Trier par nom
           </button>
           <ul class="dropdown-menu dropdown-menu-end">
             <li>
-              <a class="dropdown-item" href="#" @click.prevent="sorter = 'name'">
-                Nom
-              </a>
+              <a class="dropdown-item" href="#"> Nom </a>
             </li>
             <li>
-              <a class="dropdown-item" href="#" data-test-sorter-price @click.prevent="sorter = 'price'">
+              <a class="dropdown-item" href="#" data-test-sorter-price>
                 Prix
               </a>
             </li>
@@ -77,26 +62,50 @@ fetchProducts();
       </div>
     </div>
 
-    <div class="text-center mt-4" data-test-loading v-if="loading">
+    <div class="text-center mt-4" data-test-loading>
       <div class="spinner-border" role="status">
         <span class="visually-hidden">Chargement...</span>
       </div>
     </div>
 
-    <div class="alert alert-danger mt-4" role="alert" data-test-error v-if="error">
+    <div class="alert alert-danger mt-4" role="alert" data-test-error>
       Une erreur est survenue lors du chargement des produits.
     </div>
-
     <div class="row">
-      <div class="col-md-4 mb-4" v-for="product in sortedProducts" :key="product.id" data-test-product>
+      <div class="col-md-4 mb-4" v-for="i in 10" data-test-product :key="i">
         <div class="card">
-          <RouterLink :to="{ name: 'Product', params: { productId: product.id } }">
-            <img :src="product.pictureUrl" class="card-img-top" :alt="product.name" />
+          <RouterLink :to="{ name: 'Product', params: { productId: 'TODO' } }">
+            <img
+              src="https://picsum.photos/id/403/512/512"
+              data-test-product-picture
+              class="card-img-top"
+            />
           </RouterLink>
           <div class="card-body">
-            <h5 class="card-title">{{ product.name }}</h5>
-            <p class="card-text">{{ product.description }}</p>
-            <p class="card-text">{{ product.originalPrice }} €</p>
+            <h5 class="card-title">
+              <RouterLink
+                data-test-product-name
+                :to="{ name: 'Product', params: { productId: 'TODO' } }"
+              >
+                Machine à écrire
+              </RouterLink>
+            </h5>
+            <p class="card-text" data-test-product-description>
+              Machine à écrire vintage en parfait état de fonctionnement
+            </p>
+            <p class="card-text">
+              Vendeur :
+              <RouterLink
+                data-test-product-seller
+                :to="{ name: 'User', params: { userId: 'TODO' } }"
+              >
+                alice
+              </RouterLink>
+            </p>
+            <p class="card-text" data-test-product-date>
+              En cours jusqu'au 05/04/2026
+            </p>
+            <p class="card-text" data-test-product-price>Prix actuel : 42 €</p>
           </div>
         </div>
       </div>
