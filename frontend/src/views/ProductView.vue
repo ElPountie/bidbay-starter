79eead05cc72e5b298c66d0cb88f7c55ef8e5003
ElPountie <khalis.rabehi@etu.univ-lyon1.fr>
2023-04-03 16:41:11 +0200

    feat : detail product starting implementation (only image name and information, no user information)

--- frontend/src/views/ProductView.vue
+++ frontend/src/views/ProductView.vue
@@ -1,4 +1,5 @@
 <script setup>
+
 import { ref, computed } from "vue";
 import { useRoute, useRouter, RouterLink } from "vue-router";
 import { useAuthStore } from "../store/auth";
@@ -9,11 +10,54 @@ const route = useRoute();
 const router = useRouter();
 
 const productId = ref(route.params.productId);
+const product = ref({});
+const loading = ref(true);
+const error = ref(false);
+const countdown = ref("");
+const countdownInterval = ref(null);
 
 function formatDate(date) {
   const options = { year: "numeric", month: "long", day: "numeric" };
   return new Date(date).toLocaleDateString("fr-FR", options);
 }
+
+async function getProduct() {
+  try {
+    const response = await fetch(`http://localhost:3000/api/products/${productId.value}`);
+    product.value = await response.json();
+    loading.value = false;
+    startCountdown();
+  } catch (err) {
+    console.error(err);
+    error.value = true;
+    loading.value = false;
+  }
+}
+
+function startCountdown() {
+  if (countdownInterval.value) clearInterval(countdownInterval.value);
+  const endDate = new Date(product.value.endDate);
+  const now = new Date();
+  let diff = Math.floor((endDate - now) / 1000);
+  if (diff <= 0) {
+    countdown.value = "Vente terminée";
+    return;
+  }
+  countdownInterval.value = setInterval(() => {
+    diff--;
+    if (diff <= 0) {
+      clearInterval(countdownInterval.value);
+      countdown.value = "Vente terminée";
+      return;
+    }
+    const hours = Math.floor(diff / 3600);
+    const minutes = Math.floor((diff % 3600) / 60);
+    const seconds = diff % 60;
+    countdown.value = `${hours}h ${minutes}m ${seconds}s`;
+  }, 1000);
+}
+
+getProduct();
 </script>
 
 <template>
@@ -30,12 +74,7 @@ function formatDate(date) {
     <div class="row" data-test-product>
       <!-- Colonne de gauche : image et compte à rebours -->
       <div class="col-lg-4">
-        <img
-          src="https://picsum.photos/id/250/512/512"
-          alt=""
-          class="img-fluid rounded mb-3"
-          data-test-product-picture
-        />
+        <img :src="product.pictureUrl" alt="" class="img-fluid rounded mb-3" data-test-product-picture />
         <div class="card">
           <div class="card-header">
             <h5 class="card-title">Compte à rebours</h5>
@@ -53,15 +92,12 @@ function formatDate(date) {
         <div class="row">
           <div class="col-lg-6">
             <h1 class="mb-3" data-test-product-name>
-              Appareil photo argentique
+              {{ product.name }}
             </h1>
           </div>
           <div class="col-lg-6 text-end">
-            <RouterLink
-              :to="{ name: 'ProductEdition', params: { productId: 'TODO' } }"
-              class="btn btn-primary"
-              data-test-edit-product
-            >
+            <RouterLink :to="{ name: 'ProductEdition', params: { productId: 'TODO' } }" class="btn btn-primary"
+              data-test-edit-product>
               Editer
             </RouterLink>
             &nbsp;
@@ -73,20 +109,16 @@ function formatDate(date) {
 
         <h2 class="mb-3">Description</h2>
         <p data-test-product-description>
-          Appareil photo argentique classique, parfait pour les amateurs de
-          photographie
+          {{ product.description }}
         </p>
 
         <h2 class="mb-3">Informations sur l'enchère</h2>
         <ul>
-          <li data-test-product-price>Prix de départ : 17 €</li>
-          <li data-test-product-end-date>Date de fin : 20 juin 2026</li>
+          <li data-test-product-price>Prix de départ : {{ product.originalPrice }} €</li>
+          <li data-test-product-end-date>Date de fin : {{ formatDate(product.endDate) }}</li>
           <li>
             Vendeur :
-            <router-link
-              :to="{ name: 'User', params: { userId: 'TODO' } }"
-              data-test-product-seller
-            >
+            <router-link :to="{ name: 'User', params: { userId: 'TODO' } }" data-test-product-seller>
               alice
             </router-link>
           </li>
@@ -105,10 +137,7 @@ function formatDate(date) {
           <tbody>
             <tr v-for="i in 10" :key="i" data-test-bid>
               <td>
-                <router-link
-                  :to="{ name: 'User', params: { userId: 'TODO' } }"
-                  data-test-bid-bidder
-                >
+                <router-link :to="{ name: 'User', params: { userId: 'TODO' } }" data-test-bid-bidder>
                   charly
                 </router-link>
               </td>
@@ -127,22 +156,12 @@ function formatDate(date) {
         <form data-test-bid-form>
           <div class="form-group">
             <label for="bidAmount">Votre offre :</label>
-            <input
-              type="number"
-              class="form-control"
-              id="bidAmount"
-              data-test-bid-form-price
-            />
+            <input type="number" class="form-control" id="bidAmount" data-test-bid-form-price />
             <small class="form-text text-muted">
               Le montant doit être supérieur à 10 €.
             </small>
           </div>
-          <button
-            type="submit"
-            class="btn btn-primary"
-            disabled
-            data-test-submit-bid
-          >
+          <button type="submit" class="btn btn-primary" disabled data-test-submit-bid>
             Enchérir
           </button>
         </form>
