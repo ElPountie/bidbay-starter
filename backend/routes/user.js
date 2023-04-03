e12711372e7e62be59f6d64fe2235edb5023bd59
ElPountie <khalis.rabehi@etu.univ-lyon1.fr>
2023-04-03 16:53:41 +0200

    feats : GET /api/users/:userId implemented

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,7 +4,30 @@ import { User, Product, Bid } from '../orm/index.js'
 const router = express.Router()
 
 router.get('/api/users/:userId', async (req, res) => {
-  res.status(600).send()
+  const { userId } = req.params
+  const user = await User.findOne({
+    where: { id: userId },
+    include: [
+      {
+        as : 'products',
+        model: Product,
+        include: [
+          {
+            as : 'bids',
+            model: Bid,
+            include: [
+              {
+                as : 'bidder',
+                model: User,
+                attributes: ['id', 'username']
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  })
+  res.status(200).send(user)
 })
 
 export default router
